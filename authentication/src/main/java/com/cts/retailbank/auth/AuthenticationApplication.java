dbaa8e9039150ae5de8b265f360c3fd85b7103a0
Parichay Gupta <44670041+ParichayGupta@users.noreply.github.com>
2022-04-15 12:06:26 +0530

    added conf

---
+++ authentication/src/main/java/com/cts/retailbank/auth/AuthenticationApplication.java
@@ -0,0 +1,23 @@
+package com.cts.retailbank.auth;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * The AuthenticationMsApplication class to start the application
+ *
+ */
+@SpringBootApplication
+
+public class AuthenticationApplication {
+
+	/**
+	 * The main method for application
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationApplication.class, args);
+	}
+
+}
